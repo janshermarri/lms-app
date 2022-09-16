8ec015b57ee3240c120ed202b41673edb6fb8064
janshermarri <janshermarri@yahoo.com>
2022-09-16 13:21:24 +0500

    Minor fixes

--- pages/status/500/index.tsx
+++ pages/status/500/index.tsx
@@ -117,7 +117,7 @@ function Status500() {
               <Container maxWidth="sm">
                 <Box textAlign="center">
                   <TypographyPrimary variant="h1" sx={{ my: 2 }}>
-                    Tokyo Free Black Next.js Typescript Admin Dashboard
+                    LMS Dashboard
                   </TypographyPrimary>
                   <TypographySecondary
                     variant="h4"
--- src/components/LogoSign/index.tsx
+++ src/components/LogoSign/index.tsx
@@ -99,7 +99,7 @@ function Logo() {
 
   return (
     <TooltipWrapper
-      title="Tokyo Free Black Next.js Typescript Admin Dashboard"
+      title="LMS Dashboard"
       arrow
     >
       <LogoWrapper href="/">
